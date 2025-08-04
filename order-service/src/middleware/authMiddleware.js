9c645673bc6bd6d6968b6ca665e6ef9ed4a0b77c
codeVedang <vedangt17@gmail.com>
2025-08-04 12:57:27 +0530

    feat: add docker support and final readme

--- order-service/src/middleware/authMiddleware.js
+++ order-service/src/middleware/authMiddleware.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Attach decoded user info to the request
+        req.user = decoded; 
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Unauthorized: Invalid token' });
